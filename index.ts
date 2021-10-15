c54d9492c833117e79a6653bcdc303bb2bc752db
stephan418 <stephan418@users.noreply.github.com>
2021-10-15 13:36:56 +0000

    [create-pull-request] push formatted files

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 async function main() {
   // ... you will write your Prisma Client queries here
-  console.log("Hello world!!")
+  console.log("Hello world!!");
 }
 
 main()
   .catch((e) => {
-    throw e
+    throw e;
   })
   .finally(async () => {
-    await prisma.$disconnect()
-  })
+    await prisma.$disconnect();
+  });

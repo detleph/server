269cc33a33101c97f2e7955559fee88e38c16907
Stefan-5422 <32109571+Stefan-5422@users.noreply.github.com>
2022-03-20 17:29:25 +0100

    Update Event Endpoints to give better errors;

--- src/Routes/event.routes.ts
+++ src/Routes/event.routes.ts
@@ -1,9 +1,12 @@
 import Express from "express";
-import { addEvent, getAllEvents } from "../Controllers/event.controller";
+import { addEvent, getAllEvents, getEvent } from "../Controllers/event.controller";
+import { requireAuthentication } from "../Middleware/auth/auth";
 const router = Express.Router();
 
 router.get("/", getAllEvents);
 
-router.post("/", addEvent);
+router.get("/:eventId", getEvent);
+
+router.post("/", requireAuthentication, addEvent);
 
 export default router;

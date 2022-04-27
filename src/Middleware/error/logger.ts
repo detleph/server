9516b60d93e45b4985b79229de186f8324374f3d
Stephan <57194608+stephan418@users.noreply.github.com>
2022-04-27 17:00:36 +0200

    Add logger using winston
    + Add formats
    + Add debug and error log files

---
+++ src/Middleware/error/logger.ts
@@ -0,0 +1,39 @@
+import winston, { createLogger } from "winston";
+
+const {
+  format: { printf, colorize, combine, timestamp, json, errors, prettyPrint },
+} = winston;
+
+const defaultJsonFormat = combine(errors({ stack: true }), timestamp(), json({ space: 2 }));
+
+const customCLIFormat = printf(({ level, message, label, timestamp, stack }) => {
+  let output = `${level}${stack ? `(1/2:message)` : ""} at ${timestamp}${label ? ` (#${label})` : ""}: ${message}\n`;
+
+  if (stack) {
+    output += `\n${level}(2/2:stack)${label ? ` (#${label})` : ""}: ${stack}\n\n`;
+  }
+
+  return output;
+});
+
+export default createLogger({
+  levels: winston.config.syslog.levels,
+  format: combine(errors({ stack: true }), timestamp()),
+  transports: [
+    new winston.transports.Console({
+      level: "debug",
+      format: combine(timestamp(), colorize(), customCLIFormat),
+    }),
+    new winston.transports.File({
+      filename: "/app/error.log",
+      level: "error",
+      format: defaultJsonFormat,
+    }),
+    new winston.transports.File({
+      level: "debug",
+      filename: "/app/debug.log",
+      format: defaultJsonFormat,
+      silent: !(process.env.NODE_ENV === "development"), // Silent when not in development
+    }),
+  ],
+});

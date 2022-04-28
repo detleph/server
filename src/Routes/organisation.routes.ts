df9b47dbfda5d33159648264976b5602e1a006a0
Stephan <57194608+stephan418@users.noreply.github.com>
2022-04-28 15:04:39 +0200

    Add router to delete groups
    + Add route to delete organisations

--- src/Routes/organisation.routes.ts
+++ src/Routes/organisation.routes.ts
@@ -2,6 +2,7 @@ import express from "express";
 import eventRouter from "./event.routes";
 import {
   createOrganisation,
+  deleteOrganisation,
   getAllOrganisations,
   getAllOrganisationsWithParam,
   getOrganisation,
@@ -14,6 +15,7 @@ const router = express.Router();
 router.get("/", getAllOrganisations);
 router.get("/:pid", getOrganisation);
 router.put<"/:pid", { pid: string }>("/:pid", requireAuthentication, updateOrganisation);
+router.delete<"/:pid", { pid: string }>("/:pid", requireAuthentication, deleteOrganisation);
 
 eventRouter.get("/:eventPid/organisations", getAllOrganisationsWithParam);
 eventRouter.post<"/:eventPid/organisations", { eventPid: string }>(

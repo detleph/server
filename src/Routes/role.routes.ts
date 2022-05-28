233182c0ea771450311723a91e6896f7960c3497
Laurin <60652077+Flexla54@users.noreply.github.com>
2022-05-28 18:36:36 +0200

    added getRolesForTeam to role.routes

--- src/Routes/role.routes.ts
+++ src/Routes/role.routes.ts
@@ -1,6 +1,9 @@
 import Express from "express";
-import { assignParticipantToRole } from "../Controllers/role.controller";
+import { assignParticipantToRole, getRolesForTeam } from "../Controllers/role.controller";
 
 const router = Express.Router();
+
+//TO DO: maybe transfer getRolesForTeam to team router
+router.get<"team/:teamPid/", { teamPid: string }>("team/:teamPid/", getRolesForTeam);
 
 router.patch<"/:pid/", { pid: string }>("/:pid/", assignParticipantToRole)

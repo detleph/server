ff13a95eacbdf6502b8b3d921c8b21f60b6cf4cd
Laurin <60652077+Flexla54@users.noreply.github.com>
2022-10-20 15:50:10 +0200

    added default admin logic

---
+++ src/Middleware/auth/defaultAdmin.ts
@@ -0,0 +1,25 @@
+import prisma from "../../lib/prisma";
+import crypto from "crypto";
+import argon2 from "argon2";
+import logger from "../error/logger";
+
+require("express-async-errors");
+
+export const generateDefaultCredentials = async () => {
+  const name: string = "Detleph_" + crypto.randomBytes(3).toString("base64url");
+
+  const pw: string = crypto.randomBytes(8).toString("base64url");
+
+  await prisma.admin.upsert({
+    where: { id: 1 },
+    create: {
+      name,
+      password: await argon2.hash(pw, { type: argon2.argon2id }),
+      permission_level: "ELEVATED",
+    },
+    update: {},
+  });
+
+  logger.notice("Default admin name is  " + name);
+  logger.notice("Confidential password:  " + pw);
+};

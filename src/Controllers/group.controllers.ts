2d794cef30b9d7dc88b958be937711ab0cddb818
Laurin <60652077+Flexla54@users.noreply.github.com>
2022-12-07 16:21:49 +0100

    basic errorhandling inconsistancy fix

--- src/Controllers/group.controllers.ts
+++ src/Controllers/group.controllers.ts
@@ -77,61 +77,51 @@ interface GetGroupQueryParams {
 export const getGroup = async (req: Request<GetGroupQueryParams>, res: Response) => {
   const { pid } = req.params;
 
-  try {
-    const group: {
-      pid: string;
-      name: string;
-      organisation: { pid: string; name: string };
-      admins?: { pid: string; name: string }[];
-      participants?: { pid: string }[];
-    } | null = await prisma.group.findUnique({
-      where: { pid },
-      select: req.auth?.isAuthenticated
-        ? {
-            pid: true,
-            name: true,
-            organisation: { select: { pid: true, name: true } },
-            admins: { select: { pid: true, name: true } },
-            participants: { select: { pid: true } },
-          }
-        : basicGroup,
-    });
-
-    if (!group) {
-      return res.status(404).json(generateError(`The group with ID '${pid}' could not be found`));
-    }
-
-    return res.status(200).json({
-      type: "success",
-      payload: {
-        group: {
-          ...group,
-          organisation: {
-            ...group.organisation,
-            _links: [{ rel: "self", type: "GET", href: `/api/organisation/${group.organisation.pid}` }],
-          },
-          ...(req.auth?.isAuthenticated
-            ? {
-                admins: group.admins?.map((admin) => ({
-                  ...admin,
-                  _links: [{ rel: "self", type: "GET", href: `/api/admins/${admin.pid}` }],
-                })),
-                participants: group.participants?.map((participant) => ({
-                  ...participant,
-                  _links: [{ rel: "self", type: "GET", href: `/api/participant/${participant.pid}` }],
-                })),
-              }
-            : {}),
-        },
-      },
-    });
-  } catch (e) {
-    if (e instanceof PrismaClientUnknownRequestError) {
-      return res.status(400).json(generateError("Unknown error occured. This could be due to malformed IDs"));
-    }
+  const group: {
+    pid: string;
+    name: string;
+    organisation: { pid: string; name: string };
+    admins?: { pid: string; name: string }[];
+    participants?: { pid: string }[];
+  } | null = await prisma.group.findUnique({
+    where: { pid },
+    select: req.auth?.isAuthenticated
+      ? {
+          pid: true,
+          name: true,
+          organisation: { select: { pid: true, name: true } },
+          admins: { select: { pid: true, name: true } },
+          participants: { select: { pid: true } },
+        }
+      : basicGroup,
+  });
+  if (!group) {
+    return res.status(404).json(generateError(`The group with ID '${pid}' could not be found`));
   }
-
-  return res.status(500).json(genericError);
+  return res.status(200).json({
+    type: "success",
+    payload: {
+      group: {
+        ...group,
+        organisation: {
+          ...group.organisation,
+          _links: [{ rel: "self", type: "GET", href: `/api/organisation/${group.organisation.pid}` }],
+        },
+        ...(req.auth?.isAuthenticated
+          ? {
+              admins: group.admins?.map((admin) => ({
+                ...admin,
+                _links: [{ rel: "self", type: "GET", href: `/api/admins/${admin.pid}` }],
+              })),
+              participants: group.participants?.map((participant) => ({
+                ...participant,
+                _links: [{ rel: "self", type: "GET", href: `/api/participant/${participant.pid}` }],
+              })),
+            }
+          : {}),
+      },
+    },
+  });
 };
 
 // at: POST /api/organisations/:eventPid/groups

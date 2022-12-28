e8941f5f188e0bdd96552d8778104deb3bb18e76
La_Felx <60652077+Flexla54@users.noreply.github.com>
2022-12-28 01:06:58 +0100

    moved deleteUnverifiedTeams to event router

--- src/Routes/event.routes.ts
+++ src/Routes/event.routes.ts
@@ -1,7 +1,8 @@
 import Express from "express";
 import { string } from "zod";
 import { addEvent, deleteEvent, getAllEvents, getEvent, updateEvent } from "../Controllers/event.controller";
-import { requireAuthentication } from "../Middleware/auth/auth";
+import { requireAuthentication, requireConfiguredAuthentication } from "../Middleware/auth/auth";
+import { deleteUnverifiedTeams } from "../Controllers/team.controller";
 const router = Express.Router();
 
 router.get("/", getAllEvents);
@@ -13,5 +14,11 @@ router.post("/", requireAuthentication, addEvent);
 router.patch<"/:pid/", { pid: string }>("/:pid/", requireAuthentication, updateEvent);
 
 router.delete<"/:pid/", { pid: string }>("/:pid/", requireAuthentication, deleteEvent);
+
+router.delete<"/:pid/deleteUnverified/", {pid: string}>(
+    "/:pid/deleteUnverified/",
+    requireConfiguredAuthentication({optional: false, type: "admin"}),
+    deleteUnverifiedTeams
+  );
 
 export default router;
